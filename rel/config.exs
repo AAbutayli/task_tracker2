a1c1274c3d5dc65c77efd0d62d9d9010b81028e3
Abdul <a.abutaily@gmail.com>
2018-10-25 16:45:21 -0400

    fixing scss

--- rel/config.exs
+++ rel/config.exs
@@ -22,17 +22,6 @@ use Mix.Releases.Config,
 # when building in that environment, this combination of release
 # and environment configuration is called a profile
 
-get_secret = fn name ->
-  base = Path.expand("~/.config/task_tracker2")
-  File.mkdir_p!(base)
-  path = Path.join(base, name)
-  unless File.exists?(path) do
-    secret = Base.encode16(:crypto.strong_rand_bytes(32))
-    File.write!(path, secret)
-  end
-  String.trim(File.read!(path))
-end
-
 environment :dev do
   # If you are running Phoenix, you should make sure that
   # server: true is set and the code reloader is disabled,
@@ -42,13 +31,13 @@ environment :dev do
   # dev mode.
   set dev_mode: true
   set include_erts: false
-  set cookie: String.to_atom(get_secret.("dev_cookie"))
+  set cookie: :"%$7*3O2YOwXmg@dLVW/LplAJ3WXZdC)46A2!Ej`QY$2s|j1KMC054p&)s:d6ZW3K"
 end
 
 environment :prod do
   set include_erts: true
   set include_src: false
-  set cookie: String.to_atom(get_secret.("prod_cookie"))
+  set cookie: :"=WIurj1!e(8[wesU7*J4[Uu^vJpdQvDR|6$Nh|jX>OAPr3x:7`u5qTiFwBmJWX7~"
 end
 
 # You may define one or more releases in this file.
---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MIX_ENV=prod
+export PORT=4848
+
+echo "Stopping old copy of app, if any..."
+
+_build/prod/rel/task_tracker2/bin/task_tracker2 stop || true
+
+echo "Starting app..."
+
+# TODO: You want start.
+
+_build/prod/rel/task_tracker2/bin/task_tracker2 start
+
+# TODO: Add a cron rule or systemd service file
+#       to start your app on system boot.
+
